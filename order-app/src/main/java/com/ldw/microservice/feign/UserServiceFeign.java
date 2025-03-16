c3fd8fd47cdfc1e7e692859b8bc94a53d1c17063
ldw1986hf123 <ldw1986hf123@163.com>
2025-03-16 11:55:53 +0800

    feign调用自己看行不行

--- order-app/src/main/java/com/ldw/microservice/feign/UserServiceFeign.java
+++ order-app/src/main/java/com/ldw/microservice/feign/UserServiceFeign.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "microservice-uc" , url = "http://192.168.172.129:8081")
+@FeignClient(name = "microservice-uc" , url = "http://192.168.172.129:8001")
 public interface UserServiceFeign {
     @GetMapping("getUserById")
     String getUserById();
